5609681ae109d2330d924dac6d93ef3a680814a3
puckfried <puckfried@posteo.de>
2022-05-19 16:00:36 +0200

    solution added

---
+++ pb-prog05-if_to_tern.js
@@ -0,0 +1,65 @@
+// Rewrite this if using the conditional operator '?':
+
+/* let result;
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+} */
+
+/* _________________________________ */
+
+let a=3;
+let b=6;
+let result = (a + b < 4) ? 'Below' : 'Over';
+console.log(result)
+
+/* _________________________________ */
+
+
+
+//Rewrite the code below to use the ternary operator (?:) (you should be able to condense the if-else logic into one line).
+/* if (score > 1337)
+{
+    msg = "This is a new highscore!";
+}
+else
+{
+    msg = "You need more points to beat the highscore!";
+} */
+
+/* _________________________________ */
+
+let score = 42;
+let msg = "";
+
+msg=(score>1337)? "This is a new highscore":"You need more points to beat the highscore" 
+
+console.log(msg)
+
+/* _________________________________ */
+
+// Rewrite if..else using multiple ternary operators '?'.
+//For readability, it’s recommended to split the code into multiple lines.
+
+/* let message;
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+} */
+
+/* _________________________________ */
+let login="Director"
+
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  ''; 
+console.log(message)
+
+/* _________________________________ */
